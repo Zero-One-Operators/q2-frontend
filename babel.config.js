999bad45ce79ce1d7db6906573980e28c56eb1f0
Brian Driscoll <67210898+BrianDriscollCode@users.noreply.github.com>
2025-02-22 21:49:25 -0700

    Fix: lint

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,12 @@
 // babel.config.js
 module.exports = {
   presets: [
-    '@babel/preset-env'
-    [
-      '@babel/preset-react',
+    '@babel/preset-env'[
+      ('@babel/preset-react',
       {
         runtime: 'automatic',
-      },
-    ], 
-    '@babel/preset-typescript', 
+      })
+    ],
+    '@babel/preset-typescript',
   ],
 };
