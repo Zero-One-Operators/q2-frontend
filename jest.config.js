699d3f3600de334feaf8d5e8f9a2d9a1acbb58f1
Brian Driscoll <67210898+BrianDriscollCode@users.noreply.github.com>
2025-02-23 11:13:46 -0700

    Jest setup (#11)
    * Add: Jest configuration. Fixing: ts-config jsx auto resetting to preserve from react-jsx
    
    * Add: extra typescript config files to fix 'jsx' being auto set to preserve instead of react causeing jest to break. Next has made preserve mandatory which breaks jest, thus the workaround
    
    * fix: lint
    
    * Chore: remove extra text
    
    * Chore: removing more extra text
    
    * Fix: lint || Chore: comment test page
    
    * Fix: lint

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+
+  // Transform TypeScript files using ts-jest
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+
+  // Ignore certain node_modules packages from transformation
+  transformIgnorePatterns: ['/node_modules/(?!(your-module|other-module)/)'],
+
+  // Handle CSS, images, etc. with mock files
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+    '\\.svg$': '<rootDir>/__mocks__/fileMock.js',
+  },
+
+  // Handle TSX file extensions
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+
+  globals: {
+    'ts-jest': {
+      useBabelrc: true,
+    },
+    //Ensures react is loaded for Jest tests because of Next preserve setting
+    tsconfig: './tsconfig.test.json',
+  },
+};
---
+++ src/app/temp/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { Button, Container, Typography } from '@mui/material';
+import { useState } from 'react';
+
+export default function Temp() {
+  const [count, setCounter] = useState(0);
+
+  return (
+    <Container>
+      <Typography variant="h1">Counter: {count}</Typography>
+
+      <Button
+        onClick={() => {
+          setCounter(count + 1);
+        }}
+        variant="contained"
+        aria-label="increment"
+      >
+        Increment
+      </Button>
+    </Container>
+  );
+}
