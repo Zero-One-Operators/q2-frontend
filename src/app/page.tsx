4d86337c47249268ccf2934ae45046f4363f6945
Brian Driscoll <67210898+BrianDriscollCode@users.noreply.github.com>
2025-02-24 23:56:48 -0700

    Add: Box for chat body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,18 @@ export default function Home() {
         padding: '1em',
         backgroundColor: '#6e6eb5',
         boxSizing: 'border-box',
+        display: 'flex',
+        flexDirection: 'row'
       }}
     >
-      <SideBar />
+      <SideBar/>
+
+      <Box sx={{ backgroundColor: 'blue', width: '100%', height: '300px', color: 'white'}}>
+        {/* Put your components here, will most likely need a box*/}
+        <p> Put your header and chat components here </p>
+      </Box>
+
+      
     </Box>  
   );
 }
--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -10,7 +10,8 @@ const SideBar = () => {
     <>
         <Box sx={{ display: 'flex',
                     height: '100%',
-                    width: '100px',
+                    width: '300px',
+                    marginRight: '1em',
                     backgroundColor: 'red',
         }}>
             <p> Will be sidebar </p>
