c921577ad1479699192c9475e4c2a9be5a0810e3
Brian <bdriscoll407@gmail.com>
2025-02-27 10:32:17 -0700

    Refactor: slashing button component, adding directly to sidebar

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -2,8 +2,8 @@
 
 import { Box } from '@mui/material';
 import ReviewsOutlinedIcon from '@mui/icons-material/ReviewsOutlined';
-import { Typography } from '@mui/material';
-import NewChatButton from './NewChatButton';
+import { Typography, Button } from '@mui/material';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 
 const SideBar = () => {
   return (
@@ -46,7 +46,41 @@ const SideBar = () => {
               alignItems: 'center',
             }}
           >
-            <NewChatButton />
+            <Button
+              variant="contained"
+              sx={{
+                marginBottom: '1em',
+                width: '100%',
+                borderRadius: 0,
+                backgroundColor: '#5b616b',
+                height: '2.25em',
+              }}
+            >
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  alignContent: 'center',
+                }}
+              >
+                <AddOutlinedIcon
+                  sx={{
+                    fontSize: '20px',
+                    fontWeight: '200',
+                    margin: '1px 3px 0 0', // adjust icon because text and icon are different sizes
+                  }}
+                />
+                <Typography
+                  variant="button"
+                  fontWeight="400"
+                  fontSize="14px"
+                  sx={{ textTransform: 'none' }}
+                >
+                  New chat
+                </Typography>
+              </Box>
+            </Button>
           </Box>
         </Box>
       </Box>
