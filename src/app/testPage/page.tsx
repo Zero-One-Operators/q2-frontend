26ec1b030d434df006d3a6f169b40732c5d9d2f2
Brian Driscoll <67210898+BrianDriscollCode@users.noreply.github.com>
2025-03-02 12:30:54 -0700

    Add: first connection to backend, push 2

--- src/app/testPage/page.tsx
+++ src/app/testPage/page.tsx
@@ -1,9 +1,35 @@
+"use client";
+import { useEffect, useState } from "react";
 
+export default function TestPage() {
+  const [data, setData] = useState({});
 
-export default function testPage() {
+  // Fetch data from FastAPI
+  const getData = async () => {
+    const host = process.env.NEXT_PUBLIC_FASTAPI_HOST;
+    if (!host) {
+      console.error("FASTAPI_HOST is not defined!");
+      return;
+    }
+
+    try {
+      const response = await fetch(`${host}`); 
+      const data = await response.json(); 
+      setData(data); 
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };f
+
+  useEffect(() => {
+    getData(); 
+  }, []);
+
   return (
-   <>
-    <h1> Test Page </h1>
-   </>
+    <>
+      <h1>Test Page</h1>
+      <button onClick={getData}>Test</button>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
   );
 }
